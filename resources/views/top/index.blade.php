01d36227544ebd7cc4213faa95007902f4cf4edc
sotto97 <sotto.bw97@gmail.com>
2022-01-03 20:53:56 +0900

    トップ画面の表示変更と画像追加

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -1,40 +1,46 @@
 @include('layouts.app')
 
-<header class="">
-    <div class="flex w-full h-auto bg-yellow-600 px-10 py-2">
-        <div class="w-1/6">
-            <div class="w-20 h-20 bg-white mx-auto">
+<header class="w-full flex">
+    <div class="flex w-1/2 h-auto bg-yellow-600 px-10 py-2">
+        {{-- <div class="w-1/6">
+            <div class="w-20 h-20 bg-white mx-auto"> --}}
                 {{-- <img src="" alt=""> --}}
-            </div>
+                {{-- </div>
+        </div> --}}
+        <div class="w-5/6 text-left my-auto">
+            {{-- <p><span class="italic font-semibold text-xl">ASABEN<span><br>朝の学習習慣を身につけるオンライン学習塾</p> --}}
+            <p class="italic font-semibold text-xl">ASABEN</p>
         </div>
-        <div class="w-4/6 text-left my-auto">
-            <p><span class="font-semibold text-xl">ASABEN<span><br>朝の学習習慣を身につけるオンライン学習塾</p>
-        </div>
-        <div class="w-1/6 text-right my-auto">
+        <!-- <div class="w-1/6 text-right my-auto">
             <button class="bg-gray-900 hover:bg-gray-800 text-white text-center py-2 px-4">お問い合わせ</button>
-        </div>
+        </div> -->
+    </div>
+    <div class="w-1/2 pl-10 py-2">
+        <ul class="flex text-center">
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">ASABEN</li>
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">特徴</li>
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">カリキュラム</li>
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">コース</li>
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">ご挨拶</li>
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">入塾の流れ</li>
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">その他</li>
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">お問い合わせ</li>
+            <!-- <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">taisei.fukushima@asaben2021.com
+            </li>
+            <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">080-2389-3091</li> -->
+        </ul>
     </div>
 </header>
 
-<section id="list" class="bg-gray-100">
-    <ul class="flex text-center">
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">ASABEN</li>
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">特徴</li>
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">カリキュラム</li>
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">コース</li>
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">ご挨拶</li>
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">入塾の流れ</li>
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">その他</li>
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">taisei.fukushima@asaben2021.com
-        </li>
-        <li class="flex-1 p-2 hover:bg-gray-400 hover:text-white cursor-pointer">080-2389-3091</li>
-    </ul>
-</section>
-
-<div class="text-center w-full h-64">
-    <h2 class="italic font-semibold text-6xl text-yellow-600">ASABEN</h2>
-    <p class="italic font-semibold text-xl mt-2">朝の学習習慣で人生を豊かに</p>
-    <p class="italic font-semibold text-xl">Change your morning,Change your life.</p>
+<div class="w-full flex">
+    <div class="text-left w-1/2 h-screen">
+        <h2 class="italic font-semibold text-6xl text-yellow-600">ASABEN</h2>
+        <p class="italic font-semibold text-xl mt-2">朝の学習習慣で人生を豊かに</p>
+        <p class="italic font-semibold text-xl">Change your morning,Change your life.</p>
+    </div>
+    <div class="w-1/2">
+        <img src="{{ asset('svg/KidsStudying.svg') }}" width="60%" class="mx-auto">
+    </div>
 </div>
 
 <div class="w-full h-auto py-20 bg-yellow-600 text-center text-white">
@@ -44,7 +50,7 @@
     <p>「一生モノ」の学習習慣を身につけます</p>
 </div>
 
-<section id='news' class="h-auto py-20 bg-gray-200 text-center">
+<section id="news" class="h-auto py-20 bg-gray-200 text-center">
     <p class="text-3xl text-yellow-600">ニュース</p>
     <p class="my-4">この度、朝のオンライン塾「ASABEN」について、代表の福島が取材を受け、教育メディア「テラコヤプラス」に記事が掲載されました。<br>
         ASABENが実現したいことや強み・特徴について答えておりますので、ぜひご一読ください！</p>
